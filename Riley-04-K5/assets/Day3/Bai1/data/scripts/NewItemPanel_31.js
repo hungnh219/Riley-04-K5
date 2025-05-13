b31dc0b72fcbd2ddfebaef7023ce15d4a1eecdef
riley <hungnh219@gmail.com>
2025-05-13 09:00:51 +0700

    3321

--- Riley-04-K5/assets/Day3/Bai1/data/scripts/NewItemPanel_31.js
+++ Riley-04-K5/assets/Day3/Bai1/data/scripts/NewItemPanel_31.js
@@ -11,53 +11,28 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        this.prefabFactory = this.prefabFactory.getComponent("PrefabFactory_31");
-        if (!this.prefabFactory) {
-            console.error("Prefab Factory not found!!!");
-            return;
-        }
+            console.log('check');
+        this.prefabs = this.prefabFactory.getAllPrefabs();
+
+        console.log("prefab 0: ", this.prefabs[0])
+
+        const itemNode = cc.instantiate(this.prefabs[0]);
+
+        console.log("prefab 0 node: ", itemNode)
+        // console.log("image label: ", itemNode.getChildByName("ItemImageLabel"))
+        console.log("children: ", itemNode.children.getChildByName("ItemImageLabel"))
+
+        itemNode.children.forEach(element => {
+            element.name;
+            console.log('test ', element.name)
+            
+        });
+
     },
 
     start () {
-        console.log(this.prefabs);
-        this.initData();
-    },
+        console.log(this.prefabs[0].children)
 
-    initData() {
-        this.scrollViewContent.removeAllChildren();
-        this.prefabs = this.prefabFactory.getAllPrefabs();
-
-        this.prefabs.forEach((prefab) => {
-            const itemNode = cc.instantiate(prefab);
-
-            const imageSprite = itemNode.getChildByName('ItemImageSprite').getComponent(cc.Sprite);
-
-            if (!imageSprite) {
-                console.warn("ItemImageSprite not found in prefab:", prefab.name);
-                return;
-            }
-
-            // create node to display sprite
-            const spriteNode = new cc.Node();
-            const spriteComponent = spriteNode.addComponent(cc.Sprite);
-            spriteComponent.spriteFrame = imageSprite.spriteFrame;
-            spriteComponent.sizeMode = cc.Sprite.SizeMode.CUSTOM;
-
-            spriteNode.setContentSize(80, 80);
-            spriteNode.anchorX = 0.5;
-            spriteNode.anchorY = 0.5;
-
-            spriteNode.on('touchend', () => {
-                this.onSpriteClicked(prefab);
-            }, this);
-
-            this.scrollViewContent.addChild(spriteNode);
-        });
-    },
-  
-
-    onSpriteClicked(prefab) {
-        console.log("Sprite clicked:", prefab.name);
     },
 
     // update (dt) {},
